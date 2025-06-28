9a4a348074e3c9f102d5ab4e72313902a581dd2b
Mosacd <levanmosiashvili5@gmail.com>
2025-06-28 18:31:05 +0400

    Landing page complete, data is displayed as cards, pagination works, page number goes up in url, search works

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,11 +1,12 @@
 import img from "@/assets/5b56caff917096364317406080258eca 2.png"
 
-const Card = () => {
+const Card = ({ name } : {name: string}) => {
+
     return (
-    <div className="w-full max-w-[547px] h-[640px] bg-black py-[35px] px-[40px]">
-        <div className="w-full px-[10px] max-w-[465px] flex justify-center items-center h-[570px] border-[2px] border-[#B39D77] relative">
-            <img src={img} alt="" />
-            <div className="text-[24px] absolute -bottom-6 left-1/2 -translate-x-1/2 font-[Noto Sans] text-center bg-black w-fit px-[24px] py-[10px] text-white">Luke Skywalker</div>
+    <div className={`w-full max-w-[547px] h-[640px] bg-black py-[35px] px-[40px] rounded-xl hover:-translate-y-2 group border-2 border-transparent hover:shadow-[0px_5px_0px_#B39D77] duration-300 cursor-pointer `}>
+        <div className="w-full px-[10px] rounded-lg max-w-[465px] flex justify-center items-center h-[570px] border-[2px] border-[#B39D77] relative">
+            <img className="rounded-full" src={img} alt="" />
+            <div className="text-[24px] absolute -bottom-6 left-1/2 -translate-x-1/2 font-[Noto Sans] text-center bg-black w-fit px-[10px] py-[10px] text-white">{name}</div>
         </div>
     </div>
     )
